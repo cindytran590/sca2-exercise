15b44a52a3e2abb87e8bb624bd6498d8f7d54e18
Cindy <ctran33@bostonk12.org>
2019-07-15 16:47:16 -0400

    1 _PM solutions

--- 1_PM/filter.js
+++ 1_PM/filter.js
@@ -1,16 +1,23 @@
 // Complete the implementation below such that all elements of the array
 // that are not multiples of 7 are removed
 
-const arr = [7, 8, 15, 14, 13, 70, 44, 35, 88, 77, false, "7", "hi", null];
+const arr = [7, 8, 15, 14, 13, 70, 44, 35, 88, 77, false, "7", "hi", null]
 
 const multOf7 = el => {
   // TODO
-};
+  if (typeof el != typeof 42) {
+    return false 
+  }
+  if (el % 7 === 0) {
+      return true
+    }
+  else {
+    return false
+  }  
+//Array.prototype.filter = function(fun) {
+// TODO
+ };
 
-Array.prototype.filter = function(fun) {
-  // TODO
-};
+const newArr = arr.filter(multOf7)
 
-const newArr = arr.filter(multOf7);
-
-console.log(newArr);
+console.log(newArr)
--- 1_PM/map.js
+++ 1_PM/map.js
@@ -1,14 +1,16 @@
 // Complete the implementation of the following functions so that
 // the output is [0, 3, 6, 9, 12]
 
-const arr = [0, 1, 2, 3, 4];
+const arr = [0, 1, 2, 3, 4]
 
-const triple = () => {
-  // TODO
-};
+const triple = z => {
+  while (typeof z == typeof 42) {
+    newList === z * 3
+  }
+}
 
 Array.prototype.map = function(fun) {
   // TODO
-};
+}
 
-console.log(arr.map(triple));
+console.log(arr.map(triple))
